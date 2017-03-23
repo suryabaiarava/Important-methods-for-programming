fdaa3706394dc0d3f181e6772ada1046a35fd638
Bairava surya <surya.b800@gmail.com>
2017-03-23 14:55:35 +0530

    Update Maxsubarray.java

--- Maxsubarray.java
+++ Maxsubarray.java
@@ -10,45 +10,6 @@ import java.util.regex.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		final Scanner in = new Scanner(System.in);
 
-		// Get the no of test cases
-		final int T = in.nextInt();
-		for (int i = 0; i < T; i++) {
 
-			int maxSumElems = Integer.MIN_VALUE;
-
-			int maxSumOfContiElems = Integer.MIN_VALUE;
-
-			int sumOfPrevContiElems = 0;// calculates sum till current element
-
-			final int N = in.nextInt();
-			for (int j = 0; j < N; j++) {
-				final int elem = in.nextInt();
-
-				if (sumOfPrevContiElems < 0) {
-					sumOfPrevContiElems = 0;
-				}
-				sumOfPrevContiElems += elem;// To calculate contigous sum
-				// Logic to find non-contigous sum
-				if (elem > 0) {
-					if (maxSumElems < 0) {
-						maxSumElems = elem;
-					} else {
-						maxSumElems += elem;
-					}
-				} else {
-					if (maxSumElems < 0) {
-						maxSumElems = Math.max(elem, maxSumElems);
-					}
-				}
-
-			}
-
-			System.out.println(maxSumOfContiElems + " " + maxSumElems);
-		}
-
-		in.close();
-	}
-}
+		
